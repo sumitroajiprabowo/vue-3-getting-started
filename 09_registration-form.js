4793ffb384ff32322c37277fd09746c110d742bc
Sumitro Aji Prabowo <bowo@anakdesa.id>
2022-05-05 21:09:37 +0700

    Initial Commit

---
+++ 09_registration-form.js
@@ -0,0 +1,43 @@
+const RegistrationForm = {
+    props: ["items"],
+    template: `
+        <h3>Registration</h3>
+        <hr>
+        <form autocomplete="off" class="needs-validation" novalidate>
+            <text-input label="First Name" name="first-name" required="true" type="text"></text-input>
+            <text-input label="Last Name" name="last-name" required="true" type="text"></text-input>
+            <text-input label="Email" name="email" required="true" type="email"></text-input>
+            <text-input label="Password" name="password" required="true" type="password"></text-input>
+            <select-input label="Favourite Colour" name="color" :items="items"></select-input>
+            <check-input label="I agree to terms and conditions" required="true"></check-input>
+            <hr>
+            <input class="btn btn-outline-primary" type="submit" value="Register">
+        </form>
+    `,
+    components: {
+        'text-input': TextInput,
+        'select-input': SelectInput,
+        'check-input': CheckInput,
+    },
+    mounted() {
+        (function () {
+            'use strict'
+
+            // Fetch all the forms we want to apply custom Bootstrap validation styles to
+            var forms = document.querySelectorAll('.needs-validation')
+
+            // Loop over them and prevent submission
+            Array.prototype.slice.call(forms)
+                .forEach(function (form) {
+                form.addEventListener('submit', function (event) {
+                    if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                    }
+
+                    form.classList.add('was-validated')
+                }, false)
+                })
+            })()
+    }
+}
---
+++ 10_forms.js
@@ -0,0 +1,53 @@
+const TextInput = {
+    props: {
+        name: String,
+        type: String,
+        label: String,
+        placeholder: String,
+        required: String,
+        min: String,
+        max: String,
+        value: String,
+    },
+    template: `
+        <div class="mb-3">
+            <label :for="name" class="form-label">{{label}}</label>
+            <input
+                :type="type"
+                :name="name"
+                :placeholder="placeholder"
+                :required="required"
+                :min="min"
+                :max="max"
+                :value="value"
+                :autocomplete="name + '-new'"
+                class="form-control">
+        </div>
+    `
+}
+
+const SelectInput = {
+    props: ["items", "name", "required", "label"],
+    template: `
+        <div class="mb-3">
+            <label :for="name" class="form-label">{{label}}</label>
+            <select :id="name" class="form-select" :name="name" :required="required">
+                <option v-for="option in items" :value="option.value">
+                    {{option.text}}
+                </option>
+            </select>
+        </div>
+    `
+}
+
+const CheckInput = {
+    props: ["label", "required", "name", "value", "checked"],
+    template: `
+        <div class="form-check">
+            <input class="form-check-input" :checked="checked" :required="required" type="checkbox" :value="value" :name="name" :id="name">
+            <label class="form-check-label" :for="name">
+                {{label}}
+            </label>
+        </div>
+    `
+}
